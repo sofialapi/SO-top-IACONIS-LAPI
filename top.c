56498fe1f0703de12e1f58a3fbe8be0b72b8a073
francescaiaconis <fraiac99@gmail.com>
2022-09-12 15:59:36 +0200

    PROVA: prelevo dato da /proc

--- top.c
+++ top.c
@@ -1,1 +1,24 @@
-#progetto
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void main(int argc, char **argv) {
+    int pid;
+    sscanf(argv[1], "%d", &pid);
+    printf("pid = %d\n", pid);
+
+    char buf[1000];
+    sprintf(buf, "/proc/%d/stat", pid);
+    FILE *f = fopen(buf, "r");
+
+    int n;
+    char name[1000];
+    char stat;
+    int padre;
+    fscanf(f, "%d %s %c %d", &n, name, &stat, &padre);
+    printf("name = %s\n", name);
+    printf("stato= %c\n", stat);
+    printf("padre = %d\n", padre);
+    fclose(f);
+}
